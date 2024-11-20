166aebfeade68e3ec45fa7176d88c40b7228feb8
ericorf21 <cocop88@outlook.co.id>
2024-11-20 22:55:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'ISI LINK RAW';
+$url = 'https://raw.githubusercontent.com/ericorf21/BL/refs/heads/main/file.txt';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
